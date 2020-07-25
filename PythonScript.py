c7915ffb057f79dcaceeeb44bf92f1898652cb64
GraciaDufort <graciadufort@gmail.com>
2020-07-25 19:22:06 -0400

    Added checkJavaExists() function and made some code refactoring

--- PythonScript.py
+++ PythonScript.py
@@ -1,7 +1,23 @@
 import subprocess
 
-def checkJavaVersion():
-	return subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT)
+javaCmdResult = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode("utf-8")
+cmdResultString = javaCmdResult.partition('\n')[0]
 
-version = checkJavaVersion()	
-print(version)
+
+def checkJavaExists():
+	if "not recognized" not in cmdResultString:
+		return "Yes"
+	else:
+		return "No"
+
+def checkJavaVersion():
+	return cmdResultString
+
+
+isJavaInstalled = checkJavaExists()
+finalResult = "Is Java Installed: " + isJavaInstalled + "\n"
+
+if isJavaInstalled == "Yes":
+	finalResult += "Java Version: "+checkJavaVersion() + "\n"
+
+print(finalResult)
