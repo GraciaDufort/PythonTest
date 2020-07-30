3646c340bc35b7ce66603de5d348ac82c2572a8c
GraciaDufort <graciadufort@gmail.com>
2020-07-30 09:40:49 -0400

    Implemented java download method (but file is unreadable or broken)

--- PythonScript.py
+++ PythonScript.py
@@ -1,7 +1,5 @@
 import subprocess
-
-javaCmdResult = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode("utf-8")
-cmdResultString = javaCmdResult.partition('\n')[0]
+from urllib.request import urlopen, Request
 
 
 def checkJavaExists():
@@ -12,12 +10,29 @@ def checkJavaExists():
 
 def checkJavaVersion():
 	return cmdResultString
+	
+def download(url, filename):
+    print(url)
+    print(filename)
+    javaFile = urlopen(url)
+    with open(filename, 'wb+') as save:
+        save.write(javaFile.read())
 
+
+
+javaCmdResult = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode("utf-8")
+cmdResultString = javaCmdResult.partition('\n')[0]
 
 isJavaInstalled = checkJavaExists()
 finalResult = "Is Java Installed: " + isJavaInstalled + "\n"
 
 if isJavaInstalled == "Yes":
 	finalResult += "Java Version: "+checkJavaVersion() + "\n"
+	
+
+if isJavaInstalled == "No":
+	url = 'http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-windows-x64.exe'
+	filename = 'jdk-8u131-windows-x64.exe'
+	download(url, filename)
 
 print(finalResult)
